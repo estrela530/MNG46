74d86807ee53a2e8dbbc4b7bf92ac09bc5f2b5f8
blue666225 <fwantazi1145@gmail.com>
2021-03-10 00:42:25 +0900

    警告をなくした

--- MNG46/Assets/Script/Enemys/move2.cs
+++ MNG46/Assets/Script/Enemys/move2.cs
@@ -5,44 +5,44 @@ using UnityEngine;
 public class move2 : MonoBehaviour
 {
     // Start is called before the first frame update
-    private int aa;
-    private float dis;//プレイヤーとの距離
-    public float speed;
+    //private int aa;
+    //private float dis;//プレイヤーとの距離
+    //public float speed;
 
-    private GameObject player;//追尾する相手
+    //private GameObject player;//追尾する相手
 
 
     void Start()
     {
-        player = GameObject.Find("Player");//追尾させたいオブジェクトを書く
-        aa = 0;
+        //player = GameObject.Find("Player");//追尾させたいオブジェクトを書く
+        //aa = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        dis = Vector3.Distance(transform.position, player.transform.position);//二つの距離を計算して一定以下になれば追尾
+        //dis = Vector3.Distance(transform.position, player.transform.position);//二つの距離を計算して一定以下になれば追尾
 
-        switch (aa)
-        {
-            case 1:
+        //switch (aa)
+        //{
+        //    case 1:
 
-                break;
+        //        break;
 
-                //追う
-            case 2:
+        //        //追う
+        //    case 2:
 
-                this.transform.LookAt(this.player.transform);
+        //        this.transform.LookAt(this.player.transform);
 
-                transform.position += transform.forward * speed * Time.deltaTime;
+        //        transform.position += transform.forward * speed * Time.deltaTime;
 
-                break;
+        //        break;
 
-            case 3:
+        //    case 3:
 
-                break;
-        }
+        //        break;
+        //}
 
     }
 }
